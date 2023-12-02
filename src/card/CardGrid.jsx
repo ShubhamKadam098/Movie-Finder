63cc6a414565c0c61bfd426b96d35297a0a72956
Shubham Kadam <kadamshubham098@gmail.com>
2023-12-02 15:23:41 +0530

    Added grid for movie cards

---
+++ src/card/CardGrid.jsx
@@ -0,0 +1,17 @@
+import Card from "./Card";
+
+const CardGrid = () => {
+  return (
+    <>
+      <div className="container mb-8 grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-5 gap-y-8 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
+};
+
+export default CardGrid;
