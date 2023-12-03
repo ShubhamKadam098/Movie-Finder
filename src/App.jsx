41fa0238d198e56ced3271e8821b5e945f2231f5
Shubham Kadam <kadamshubham098@gmail.com>
2023-12-03 20:07:38 +0530

    Added title for card grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import CardGrid from "./card/CardGrid";
+import GridTitle from "./card/GridTitle";
+import Footer from "./footer/Footer";
 import Header from "./header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <GridTitle />
+      <CardGrid />
+      <Footer />
     </>
   );
 }
---
+++ src/card/GridTitle.jsx
@@ -0,0 +1,9 @@
+const GridTitle = () => {
+  return (
+    <div className="pl-16 border border-x-0 border-zinc-400 mx-auto py-6 my-6 bg-slate-800 ">
+      <h1 className="font-semibold text-2xl text-yellow-500">Popular Movies</h1>
+    </div>
+  );
+};
+
+export default GridTitle;
