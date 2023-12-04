1bbf8886e7eb0eb7d4bb7b796ed8e96066a93c3b
Shubham Kadam <kadamshubham098@gmail.com>
2023-12-04 15:45:08 +0530

    Updated card formmating

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
   const [rating, setRating] = useState(4.5);
 
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:scale-105 hover:ease-in-out hover:duration-150">
       <a href="#">
         <img
           className="rounded-t-lg"
@@ -13,31 +13,27 @@ const Card = () => {
           alt=""
         />
       </a>
-      <div className="p-5">
+      <div className="p-3">
         <a href="#">
-          <h5 className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-white">
-            Oppenheimer (2023)
+          <h5 className="mb-2 text-base font-semibold tracking-tight text-gray-900 dark:text-white">
+            Oppenheimer
           </h5>
         </a>
-        <p className="mb-3 font-sm text-sm text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"
-        >
-          {rating} / 5.0
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-          </svg>
-        </a>
+        <div className="">
+          <p className="text-gray-300">2023</p>
+          <div className="inline-flex items-center px-2 py-1 text-xs font-medium text-center text-white bg-yellow-400 rounded-lg hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-yellow-400 dark:hover:bg-yellow-500 dark:focus:ring-yellow-600">
+            {rating} / 5.0
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/card/CardGrid.jsx
+++ src/components/card/CardGrid.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 const CardGrid = () => {
   return (
     <>
-      <div className="container mb-8 grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-5 gap-y-8 ">
+      <div className="container mb-8 grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-x-5 gap-y-8 ">
         <Card />
         <Card />
         <Card />
