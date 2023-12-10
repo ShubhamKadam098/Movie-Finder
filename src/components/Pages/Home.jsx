f530f49d6dbe0aaee4852d5b8a6d90d288785818
Shubham Kadam <kadamshubham098@gmail.com>
2023-12-10 15:18:11 +0530

    Added function to fetch popular movies list

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CardGrid from "../card/CardGrid";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    fetchPopularMovies();
+  }, []);
+
+  async function fetchPopularMovies() {
+    try {
+      const response = await fetch(
+        `${
+          import.meta.env.VITE_BASE_URL
+        }discover/movie?sort_by=popularity.desc${import.meta.env.VITE_API_KEY}`
+      );
+      console.log(import.meta.VITE_API_URL);
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      const data = await response.json();
+      setPopularMovies(data.results);
+      console.log(data.results);
+    } catch (error) {
+      console.error("There was a problem fetching the data:", error);
+    }
+  }
+
+  return (
+    <>
+      <div className="bg-[#1F2937] py-6 rounded-lg pl-4 my-10">
+        <h1 className="font-semibold text-2xl text-yellow-500 ">Popular</h1>
+      </div>
+      <CardGrid movieList={popularMovies} />
+    </>
+  );
 };
 
 export default Home;
