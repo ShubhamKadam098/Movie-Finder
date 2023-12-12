57c07817acff34c6041bd1c90ca67d5c86db9a7b
Shubham Kadam <kadamshubham098@gmail.com>
2023-12-12 22:16:50 +0530

    Updated card grid to display movie with poster only

--- src/components/card/CardGrid.jsx
+++ src/components/card/CardGrid.jsx
@@ -1,12 +1,12 @@
 import Card from "./Card";
 
-const CardGrid = ({ movieList }) => {
+const CardGrid = ({ movieList = [] }) => {
   return (
     <>
       <div className="container mb-8 grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-x-5 gap-y-8 ">
-        {movieList.map((movie) => (
-          <Card key={movie.id} movieDetails={movie} />
-        ))}
+        {movieList.map((movie) =>
+          movie.poster_path ? <Card key={movie.id} movieDetails={movie} /> : ""
+        )}
       </div>
     </>
   );
